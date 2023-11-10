89d1b72db47c304a3a571f29c2233fda19ccd3fa
Dorian <dorian.goral@icloud.com>
2023-11-10 21:38:50 +0100

    Zad 4

--- src/Hello.java
+++ src/Hello.java
@@ -5,6 +5,7 @@ public class Hello
     public static void main(String[] args)
     {
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("What is your name? ");
         String name = scanner.nextLine();
         System.out.print("What is your age? ");
