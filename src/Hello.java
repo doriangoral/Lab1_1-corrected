3d9f305628584fd9fe2336352fa3fdcd2c9195ac
Dorian <dorian.goral@icloud.com>
2023-11-10 21:32:24 +0100

    Zad 3

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,12 @@
 public class Hello
 {
+    static String name = "Dorian";
+    static int age = 23;
+    static String major = "IT";
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Major: " + major);
     }
 }
